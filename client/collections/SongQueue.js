8dd27567c4dd702d94243888290a3a608278ed77
chikeichan <chikeichan@gmail.com>
2014-12-31 15:07:52 -0800

    Added playFirst in queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,14 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    this.on('add',function(){
+      if(this.models.length === 1){
+        this.playFirst();
+      }
+    })
+  },
 
+  playFirst: function(){
+    this.at(0).play();
+  }
 });
